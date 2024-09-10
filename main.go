9a05dd30d56e49b5b4ef027c86140bc8815c8430
Ell534 <elliot94b@gmail.com>
2024-09-10 17:45:08 +0100

    refactor metrics into own file and complete Ch2 Pt3

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/healthz", handleReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("GET /healthz", handleReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
 	mux.HandleFunc("/reset", apiCfg.handleResetMetrics)
 
 	myServer := &http.Server{
@@ -26,17 +26,4 @@ func main() {
 
 	fmt.Println("Listening on port 8080...")
 	log.Fatal(myServer.ListenAndServe())
-}
-
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileServerHits++
-		next.ServeHTTP(w, req)
-	})
 }
