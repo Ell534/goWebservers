0fa6d45dd219a8d8e1e4cdb7fa167471c3a260ff
Ell534 <elliot94b@gmail.com>
2024-09-10 17:23:49 +0100

    complete Ch2 Pt1 Routing

--- main.go
+++ main.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, req)
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{fileServerHits: 0}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", handleHealthz)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleResetMetrics)
 
 	myServer := &http.Server{
 		Addr:    ":8080",
@@ -24,4 +40,19 @@ func handleHealthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hitsString := strconv.Itoa(cfg.fileServerHits)
+	w.Write([]byte("Hits: " + hitsString))
+}
+
+func (cfg *apiConfig) handleResetMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits = 0
+	hitsString := strconv.Itoa(cfg.fileServerHits)
+	w.Write([]byte("Hits: " + hitsString))
 }
