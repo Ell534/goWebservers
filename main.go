f36619124b8169bb83e0775bc14409591a0abfd4
Ell534 <elliot94b@gmail.com>
2024-11-13 18:01:28 +0000

    complete ch5pt3

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/Ell534/goWebservers/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits int
+	DB             *database.Queries
 }
 
 func main() {
-	apiCfg := apiConfig{fileServerHits: 0}
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("error opening database: ", err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		fileServerHits: 0,
+		DB:             dbQueries,
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
 	mux.HandleFunc("GET /api/healthz", handleReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
+
 	mux.HandleFunc("GET /api/reset", apiCfg.handleResetMetrics)
 	mux.HandleFunc("POST /api/validate_chirp", handleValidate)
 
