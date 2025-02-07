170633d176a9bf164a31df1a52bc86224c050dca
Ell534 <elliot94b@gmail.com>
2025-02-07 17:24:11 +0000

    restart

--- reset.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-func (cfg *apiConfig) handleResetMetrics(w http.ResponseWriter, req *http.Request) {
-	cfg.fileServerHits = 0
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0!"))
-}
