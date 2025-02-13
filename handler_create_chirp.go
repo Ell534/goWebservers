0ed94d04d799e6dfba7b2d2f4d14719810377e20
Ell534 <elliot94b@gmail.com>
2025-02-13 17:23:28 +0000

    add remainder of file for last commit

---
+++ handler_create_chirp.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Ell534/goWebservers/internal/database"
+	"github.com/google/uuid"
+)
+
+type ValidChirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	newChirp := parameters{}
+	err := decoder.Decode(&newChirp)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode chirp", err)
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(newChirp.Body) > maxChirpLength {
+		respondWithError(w, 400, "Chirp length exceeds 140 characters", nil)
+		return
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	cleaned := cleanChirp(newChirp.Body, badWords)
+
+	cleanedChirp := database.CreateChirpParams{
+		Body:   cleaned,
+		UserID: newChirp.UserID,
+	}
+
+	validChirp, err := cfg.db.CreateChirp(r.Context(), cleanedChirp)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't create chirp", err)
+	}
+
+	response := ValidChirp{
+		ID:        validChirp.ID,
+		CreatedAt: validChirp.CreatedAt,
+		UpdatedAt: validChirp.UpdatedAt,
+		Body:      validChirp.Body,
+		UserID:    validChirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusCreated, response)
+}
+
+func cleanChirp(chirp string, badWords map[string]struct{}) string {
+	words := strings.Split(chirp, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidate)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	server := &http.Server{
