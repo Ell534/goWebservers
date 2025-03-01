9ee548eed94b8a120dd947209924d9ee280277d0
Ell534 <elliot94b@gmail.com>
2025-03-01 15:40:33 +0000

    complete CH5L09 get all chirps endpoint

--- handler_get_all_chirps.go
+++ handler_get_all_chirps.go
@@ -5,5 +5,21 @@ import (
 )
 
 func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	allChirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not retrieve all chirps", err)
+	}
 
+	chirps := []ValidChirp{}
+	for _, chirp := range allChirps {
+		chirps = append(chirps, ValidChirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
